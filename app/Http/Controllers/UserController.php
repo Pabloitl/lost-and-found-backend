da1529574de8afc4abcd45cc37557cdc53d390b3
Pablo <18240248@leon.tecnm.mx>
2021-07-18 12:45:02 -0500

    feat: Implemented logic of controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Campus;
+use App\Models\Carrera;
+use App\Models\Ciudad;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // curl -X POST -F 'nombre=test' -F 'apellido=test' -F 'correoElectronico=test@test.com' -F 'contrasena=password' -F 'nombreDeUsuario=test' -F 'fotoDePerfil=@' localhost:8000/api/usuarios/
+
+        // TODO: Validacion del captcha
+        $request->validate([
+            'fotoDePerfil' => 'required'
+        ]);
+
+        // TODO: Borrar creacion de otros modelos
+        $carrera_datos = ['nombre' => 'carrera', 'borrado' => false];
+        $carrera = new Carrera($carrera_datos);
+        $carrera->save();
+        $ciudad_datos = ['nombre' => 'ciudad', 'entidadFederativa' => 'federacion', 'borrado' => false];
+        $ciudad = new Ciudad($ciudad_datos);
+        $ciudad->save();
+        $campus_datos = ['nombre' => 'campus', 'borrado' => false];
+        $campus = new Campus($campus_datos);
+        $campus->ciudad()->associate($ciudad);
+        $campus->save();
+
+        // TODO: Si la imagen esta vacia
+        $fotoDePerfil = $request->file('fotoDePerfil')->store('fotos_perfil');
+
+        $datos_usuario = [
+            'nombre'            => $request->nombre,
+            'apellido'          => $request->apellido,
+            'correoElectronico' => $request->correoElectronico,
+            'contrasena'        => $request->contrasena,
+            'nombreDeUsuario'   => $request->nombreDeUsuario,
+            'borrado'           => false,
+            'usuarioCreacion'   => 0,
+            'fotoDePerfil'      => $fotoDePerfil
+        ];
+
+        $usuario = new User($datos_usuario);
+        $usuario->carrera()->associate($carrera);
+        $usuario->campus()->associate($campus);
+        $usuario->save();
+
+        return response($usuario, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/* Route::middleware('auth:api')->get('/user', function (Request $request) { */
+/*     return $request->user(); */
+/* }); */
+
+/* Route::post('/usuarios', [UserController::class, 'store']); */
+Route::apiResource('usuarios', 'App\Http\Controllers\UserController');
+/* Route::get('/usuarios', function () { */
+/*     return "Hi"; */
+/* }); */
